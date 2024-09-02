0d6eb8d0893cd66f5db60ba0ece3b402c211b17e
Sharmi <tsharmi12341234@gmail.com>
2024-09-02 14:19:39 +0530

    done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'e-commerce Chatbot',
+    version= '0.0.0',
+    author= 'tamada sharmi',
+    author_email= 'tsharmi12341234@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
---
+++ store_inexes.py
@@ -0,0 +1,22 @@
+
+import os
+import pinecone
+from tqdm import tqdm
+from dotenv import load_dotenv
+import src.helper as helper
+from langchain_pinecone import PineconeVectorStore
+
+def store_vectors():
+    """
+    The function `store_vectors` extracts data from PDFs, creates text chunks, fetches an embedding
+    model, sets up a connection to Pinecone Database, and stores the data in the database.
+    """
+    chunks = helper.get_chunks_from_pdf(path="data")
+    embeddings = helper.get_embeddings()
+    index = helper.pinecone_init()
+    done = helper.store_data(text_chunks=chunks, embeddings=embeddings, index=index)
+    if done == False:
+        exit()
+
+if __name__ == "__main__":
+    store_vectors()
